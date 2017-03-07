1375912a499b811f59e30acb4c8aeaa5ddeb081c
llpin <llpin07@163.com>
2017-03-07 10:38:20 +0800

    Initial commit

---
+++ src/main/java/center/IcheckCenterApplication.java
@@ -0,0 +1,12 @@
+package center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcheckCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcheckCenterApplication.class, args);
+	}
+}
